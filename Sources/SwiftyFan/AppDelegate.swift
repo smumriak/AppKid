9c887eca0b6df16aeb9959acc30b408be4df6c30
Serhii Mumriak <sergiy.mumryak@gmail.com>
2021-03-28 21:56:48 -0700

    Remove VulkanViewController

--- Sources/SwiftyFan/AppDelegate.swift
+++ Sources/SwiftyFan/AppDelegate.swift
@@ -12,7 +12,6 @@ class AppDelegate: ApplicationDelegate {
     func application(_ application: Application, didFinishLaunchingWithOptions launchOptions: [Application.LaunchOptionsKey: Any]? = nil) -> Bool {
         let window = Window(contentRect: CGRect(x: 0.0, y: 0.0, width: 400.0, height: 400.0))
         window.rootViewController = RootViewController()
-//        window.rootViewController = VulkanViewController()
 
         application.add(window: window)
 
