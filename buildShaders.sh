4372eac385f89be6b6a95559f25d754f79bba9c8
Serhii Mumriak <sergiy.mumryak@gmail.com>
2021-06-16 00:20:14 -0700

    Improve shaders build script by using derived data folder instead of repository.

--- buildShaders.sh
+++ buildShaders.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-resourcesPath="./AppKid/Sources/AppKid/Resources"
-shadersSourceCodePath="$resourcesPath/ShaderSources"
-shadersBinariesCodePath="$resourcesPath/ShaderBinaries"
-shaderHeaderSearchPath="./AppKid/Sources/LayerRenderingData/include"
+arch=`uname`
+
+resourcesPath=./AppKid/Sources/AppKid/Resources
+shadersSourceCodePath=$resourcesPath/ShaderSources
+shadersBinariesCodePath=$resourcesPath/ShaderBinaries
+importHeaderSearchPath=./AppKid/Sources/LayerRenderingData/include
+
+glslImporterBuildPath=~/Library/Developer/Xcode/DerivedData/glslImporter-$arch
+appKidShadersBuildPath=~/Library/Developer/Xcode/DerivedData/AppKidShaders-$arch
+
+mkdir -p $appKidShadersBuildPath
 
 find $shadersSourceCodePath -name "*.volcano" | while read file; do
     shaderName=`basename $file .volcano`
-    swift run --build-path ~/Library/Developer/Xcode/DerivedData/glslImporter-Linux glslImporter $file -I "$shaderHeaderSearchPath" || exit 1
-    glslc "$file.glsl" -o "$shadersBinariesCodePath/$shaderName.spv" || exit 1
+    outputAfterImport=$appKidShadersBuildPath/$shaderName.volcano.glsl
+    swift run --build-path $glslImporterBuildPath glslImporter $file -I $importHeaderSearchPath -o $outputAfterImport || exit 1
+    glslc $outputAfterImport -I $shadersSourceCodePath -o $shadersBinariesCodePath/$shaderName.spv|| exit 1
 done || exit 1
-
-rm -rf $shadersSourceCodePath/*.volcano.glsl
 
 exit 0
--- clean.sh
+++ clean.sh
@@ -3,4 +3,5 @@ rm -rf ~/Library/Developer/Xcode/DerivedData/SwiftyFan-`uname`
 rm -rf ~/Library/Developer/Xcode/DerivedData/SwiftyFanTests-`uname`
 rm -rf ~/Library/Developer/Xcode/DerivedData/SwiftyFan-arm64
 rm -rf ~/Library/Developer/Xcode/DerivedData/glslImporter-`uname`
+rm -rf ~/Library/Developer/Xcode/DerivedData/AppKidShaders-`uname`
 rm AppKid/Sources/AppKid/Resources/ShaderBinaries/*.spv
