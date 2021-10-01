d578d0e44b0d851bab3b7eb054f0c126aeeb4caf
Serhii Mumriak <sergiy.mumryak@gmail.com>
2021-09-30 21:10:21 -0700

    Tiny foundation sycnrhonization: synchronized helper function and propperty wrpapper on top of it

---
+++ TinyFoundation/Sources/TinyFoundation/FoundationUtilities/Synchronization.swift
@@ -0,0 +1,35 @@
+//
+//  Synchronization.swift
+//  TinyFoundation
+//
+//  Created by Serhii Mumriak on 26.09.2021.
+//
+
+import Foundation
+
+public extension NSLocking {
+    func synchronized<T>(_ body: () throws -> T) rethrows -> T {
+        self.lock()
+        defer { self.unlock() }
+
+        return try body()
+    }
+}
+
+@propertyWrapper public struct Synchronized<Value> {
+    private var value: Value
+    private let lock = NSLock()
+
+    public init(wrappedValue value: Value) {
+        self.value = value
+    }
+
+    public var wrappedValue: Value {
+        get {
+            lock.synchronized { value }
+        }
+        set {
+            lock.synchronized { value = newValue }
+        }
+    }
+}
