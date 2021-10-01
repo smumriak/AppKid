c4e0cc172ee4ec1cfda4a666232cc64b11e0dd43
Serhii Mumriak <sergiy.mumryak@gmail.com>
2021-09-30 21:17:06 -0700

    Work in progress for dynamic parsing of vulkan structure chain. Because vulkan spec is dynamic

---
+++ Volcano/Sources/Volcano/Utilities/VulkanStructureChainParsing/VulkanStructureChainParser.swift
@@ -0,0 +1,47 @@
+//
+//  VulkanStructureChainParser.swift
+//  Volcano
+//
+//  Created by Serhii Mumriak on 23.07.2021.
+//
+
+import Foundation
+import TinyFoundation
+import CVulkan
+
+public struct VulkanStructureChainParser {
+    let values: [VkStructureType: UnsafeRawPointer]
+
+    init(chain: UnsafeRawPointer?) {
+        var nextElement = chain
+        var values: [VkStructureType: UnsafeRawPointer] = [:]
+
+        // while let element = nextElement {
+        //     let genericVylkanStructure = GenericVulkanStructureFromChainElement(element)
+
+        //     values[genericVylkanStructure.type] = element
+
+        //     nextElement = genericVylkanStructure.next
+        // }
+
+        self.values = values
+    }
+
+    func value<Result: VulkanStructure>() -> Result? {
+        if let pointer = values[Result.type] {
+            let reboundPointer = pointer.assumingMemoryBound(to: Result.self)
+
+            return reboundPointer.pointee
+        } else {
+            return nil
+        }
+    }
+
+    func valueOrNew<Result: VulkanStructure>() -> Result {
+        if let result: Result = value() {
+            return result
+        } else {
+            return .new()
+        }
+    }
+}
