3ed7a3850a1fa71e9c6659a5e9e35fa0b31b7604
Serhii Mumriak <sergiy.mumryak@gmail.com>
2022-08-28 19:03:08 -0700

    A little bit more readability in Responder.swift

--- AppKid/Sources/AppKid/Responder/Responder.swift
+++ AppKid/Sources/AppKid/Responder/Responder.swift
@@ -12,22 +12,69 @@ open class Responder {
 
     internal func responderWindow() -> Window? { return nil }
 
-    open func mouseDown(with event: Event) { nextResponder?.mouseDown(with: event) }
-    open func rightMouseDown(with event: Event) { nextResponder?.rightMouseDown(with: event) }
-    open func otherMouseDown(with event: Event) { nextResponder?.otherMouseDown(with: event) }
-    open func mouseUp(with event: Event) { nextResponder?.mouseUp(with: event) }
-    open func rightMouseUp(with event: Event) { nextResponder?.rightMouseUp(with: event) }
-    open func otherMouseUp(with event: Event) { nextResponder?.otherMouseUp(with: event) }
-    open func mouseMoved(with event: Event) { nextResponder?.mouseMoved(with: event) }
-    open func mouseDragged(with event: Event) { nextResponder?.mouseDragged(with: event) }
-    open func rightMouseDragged(with event: Event) { nextResponder?.rightMouseDragged(with: event) }
-    open func otherMouseDragged(with event: Event) { nextResponder?.otherMouseDragged(with: event) }
-    open func scrollWheel(with event: Event) { nextResponder?.scrollWheel(with: event) }
-    open func mouseEntered(with event: Event) { nextResponder?.mouseEntered(with: event) }
-    open func mouseExited(with event: Event) { nextResponder?.mouseExited(with: event) }
-    open func keyDown(with event: Event) { nextResponder?.keyDown(with: event) }
-    open func keyUp(with event: Event) { nextResponder?.keyUp(with: event) }
-    open func flagsChanged(with event: Event) { nextResponder?.flagsChanged(with: event) }
+    open func mouseDown(with event: Event) {
+        nextResponder?.mouseDown(with: event)
+    }
+
+    open func rightMouseDown(with event: Event) {
+        nextResponder?.rightMouseDown(with: event)
+    }
+
+    open func otherMouseDown(with event: Event) {
+        nextResponder?.otherMouseDown(with: event)
+    }
+
+    open func mouseUp(with event: Event) {
+        nextResponder?.mouseUp(with: event)
+    }
+
+    open func rightMouseUp(with event: Event) {
+        nextResponder?.rightMouseUp(with: event)
+    }
+
+    open func otherMouseUp(with event: Event) {
+        nextResponder?.otherMouseUp(with: event)
+    }
+
+    open func mouseMoved(with event: Event) {
+        nextResponder?.mouseMoved(with: event)
+    }
+
+    open func mouseDragged(with event: Event) {
+        nextResponder?.mouseDragged(with: event)
+    }
+
+    open func rightMouseDragged(with event: Event) {
+        nextResponder?.rightMouseDragged(with: event)
+    }
+
+    open func otherMouseDragged(with event: Event) {
+        nextResponder?.otherMouseDragged(with: event)
+    }
+
+    open func scrollWheel(with event: Event) {
+        nextResponder?.scrollWheel(with: event)
+    }
+
+    open func mouseEntered(with event: Event) {
+        nextResponder?.mouseEntered(with: event)
+    }
+
+    open func mouseExited(with event: Event) {
+        nextResponder?.mouseExited(with: event)
+    }
+
+    open func keyDown(with event: Event) {
+        nextResponder?.keyDown(with: event)
+    }
+
+    open func keyUp(with event: Event) {
+        nextResponder?.keyUp(with: event)
+    }
+
+    open func flagsChanged(with event: Event) {
+        nextResponder?.flagsChanged(with: event)
+    }
 
     open var canBecomeFirstResponder: Bool { return false }
     open var canResignFirstResponder: Bool { return true }
